c6b4b6c9aeafd7c32ba47e0585ebf79f4dd243ac
Hyeseong Kim <hey@hyeseong.kim>
2023-11-01 09:30:05 +0900

    setup database

--- mattermost.tf
+++ mattermost.tf
@@ -18,3 +18,25 @@ resource "cloudflare_r2_bucket" "mattermost_bucket" {
   account_id = data.cloudflare_accounts.me.id
   name       = "mattermost"
 }
+
+resource "neon_project" "mattermost" {
+  name = "mattermost"
+}
+
+resource "neon_branch" "mattermost_main" {
+  project_id = neon_project.mattermost.id
+  name       = "main"
+}
+
+resource "neon_role" "mattermost_admin" {
+  project_id = neon_project.mattermost.id
+  branch_id  = neon_branch.mattermost_main.id
+  name       = "admin"
+}
+
+resource "neon_database" "mattermost_db" {
+  project_id = neon_project.mattermost.id
+  branch_id  = neon_branch.mattermost_main.id
+  owner_name = neon_role.mattermost_admin.name
+  name       = "mattermost"
+}
